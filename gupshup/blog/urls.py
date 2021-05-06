d3e33d318ddbe505dc3d9d7585caec96353048e6
kashish1211 <kashish.h@somaiya.edu>
2021-05-06 14:33:25 +0530

    Added Report

--- gupshup/blog/urls.py
+++ gupshup/blog/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('post/search/', SearchResultView.as_view(), name='search-result'),
-    path('about/', views.about, name='blog-about'),
+    path('about/', views.about, name='about'),
 ]
