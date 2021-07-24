67acef40e2f903bfbcfec02dd77de7f6a862f604
kashish1211 <kashish.h@somaiya.edu>
2021-07-24 23:44:07 +0530

    added conditional navbar

--- gupshup/blog/static/blog/carousel/js/script.js
+++ gupshup/blog/static/blog/carousel/js/script.js
@@ -184,10 +184,7 @@ jQuery(function ($) {
 						myShuffle.filter(input.value);
 					}
 				});
-				$('.shuffle-btn-group label').on('click', function () {
-					$('.shuffle-btn-group label').removeClass('active');
-					$(this).addClass('active');
-				});
+				
 			}
 		}
 		projectShuffle();
