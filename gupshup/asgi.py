dd4be6f4705ef7cfad778031e7dfedd43f4aa3f6
kashish1211 <kashish.h@somaiya.edu>
2021-08-04 17:50:58 +0530

    nc

--- gupshup/asgi.py
+++ gupshup/asgi.py
@@ -1,22 +1,12 @@
 import os
 import django
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
+from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gupshup.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gupshup.settings')
+
 django.setup()
-django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_default_application()
 
 
 
