214e0edaf0754d86a95b76321ea21aa827bb0fcd
kashish1211 <kashish.h@somaiya.edu>
2021-08-04 17:14:41 +0530

    bug fixed
    Co-authored-by: Rushabh Gandhi <rushabhgandhi13@gmail.com>
    Co-authored-by: HarshitJain28 <HarshitJain28@users.noreply.github.com>

--- gupshup/settings.py
+++ gupshup/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django
+django.setup()
 
 from pathlib import Path
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
