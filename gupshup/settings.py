f6feaedf68a9fba802c0d357894c176d5046f7fa
kashish1211 <kashish.h@somaiya.edu>
2021-08-07 14:57:13 +0530

    email verification added
    Co-authored-by: Rushabh Gandhi <rushabhgandhi13@gmail.com>
    Co-authored-by: HarshitJain28 <HarshitJain28@users.noreply.github.com>

--- gupshup/settings.py
+++ gupshup/settings.py
@@ -200,8 +200,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kashish.1211.kh@gmail.com'
-EMAIL_HOST_PASSWORD = 'zoxpnrlarcdzlsdx'
+# EMAIL_HOST_USER = 'kashish.1211.kh@gmail.com'
+# EMAIL_HOST_PASSWORD = 'zoxpnrlarcdzlsdx'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
 
