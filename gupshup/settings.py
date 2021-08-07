e90520ef763b404be7cfd2eb9ffafa65975d88a6
kashish1211 <kashish.h@somaiya.edu>
2021-08-07 16:03:44 +0530

    added postgres

--- gupshup/settings.py
+++ gupshup/settings.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -278,3 +278,4 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': '_Wzbs0CyzS1kFy5qkCW1xMc7NGA',
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+django_heroku.settings(locals())
