16406a8c647aac8929b4524765430533d90cf293
kashish1211 <kashish.h@somaiya.edu>
2021-08-07 23:22:30 +0530

    nc

--- users/models.py
+++ users/models.py
@@ -22,18 +22,18 @@ college_choices = (
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	image = models.ImageField(default='default2_yyfd5t.png"',upload_to='profile_pics')
+	image = models.ImageField(default='default2_gc0qdx.png"',upload_to='profile_pics')
 	college = models.CharField(max_length = 20,choices=college_choices,default='KJSCE',blank=True, null=True)
 	about = RichTextField(blank=True, null=True)
 	followed_category = models.ManyToManyField(Category,related_name='followed_category',blank=True, null=True)
 
-	image_url = models.URLField(default = "https://res.cloudinary.com/hydryqxr6/image/upload/v1628358042/default2_yyfd5t.png")
+	image_url = models.URLField(default = "https://res.cloudinary.com/hydryqxr6/image/upload/v1628358569/media/default2_gc0qdx.png")
 	def __str__(self):
 		return f'{self.user.username} Profile '
 
 
 	def get_remote_image(self):
-		if self.image_url and self.image == 'default2_yyfd5t.png"':
+		if self.image_url and self.image == 'default2_gc0qdx.png"':
 			result = urllib.request.urlretrieve(self.image_url)
 			self.image.save(
 					os.path.basename(self.image_url),
